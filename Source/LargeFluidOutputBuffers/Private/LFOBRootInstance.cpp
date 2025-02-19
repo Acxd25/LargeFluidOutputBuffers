2cf56b6ff8942f92d3ad48097f644c1b2c2dd5f1
Joanna Hammond <JoannaHammond@users.noreply.github.com>
2025-02-19 19:34:56 +0000

    m

--- Source/LargeFluidOutputBuffers/Private/LFOBRootInstance.cpp
+++ Source/LargeFluidOutputBuffers/Private/LFOBRootInstance.cpp
@@ -7,12 +7,10 @@ void ULFOBRootInstance::DispatchLifecycleEvent(ELifecyclePhase Phase)
 {
 	Super::DispatchLifecycleEvent(Phase);
 
-    UE_LOG(LogLFOB, Display, TEXT("LFOB TEST"));
-
 	if (Phase == ELifecyclePhase::POST_INITIALIZATION)
     {
         UE_LOG(LogLFOB, Display, TEXT("Attempting CDO on Build_OilRefinery."));
-        if (const TSubclassOf<AFGBuildableFactory> BPBuildableOilRefinery = LoadClass<AFGBuildableFactory>(NULL, TEXT("/Game/FactoryGame/Buildable/Factory/OilRefinery/Build_OilRefnery.Build_OilRefnery_C")))
+        if (const TSubclassOf<AFGBuildableFactory> BPBuildableOilRefinery = LoadClass<AFGBuildableFactory>(NULL, TEXT("/Game/FactoryGame/Buildable/Factory/OilRefinery/Build_OilRefinery.Build_OilRefinery_C")))
         {
             AFGBuildableFactory* buildableFactoryCDO = BPBuildableOilRefinery.GetDefaultObject();
             buildableFactoryCDO->mFluidStackSizeMultiplier = 2;
